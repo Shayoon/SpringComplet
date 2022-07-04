e7b4134fa6d82b3c0ef176299cbeec7edc89b4ed
Shayoon <quentin.degorgue@gmail.com>
2022-07-04 16:57:00 +0200

    recup de la correction

--- src/main/java/aston/correction/controller/PersonneController.java
+++ src/main/java/aston/correction/controller/PersonneController.java
@@ -6,10 +6,7 @@ import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,12 @@ public class PersonneController {
     @ResponseStatus(HttpStatus.OK)
     public List<Personne> findAll(){
         return this.personneService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Personne findById(@PathVariable Long id){
+
     }
 
 
