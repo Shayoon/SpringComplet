1f017f0aaf1a7f28c69cebe368d6fd526d5bac9b
Shayoon <quentin.degorgue@gmail.com>
2022-07-05 11:29:54 +0200

    Controller + service personnes

--- src/main/java/aston/correction/controller/PersonneController.java
+++ src/main/java/aston/correction/controller/PersonneController.java
@@ -1,9 +1,12 @@
 package aston.correction.controller;
 
+import aston.correction.entity.Formateur;
 import aston.correction.entity.Personne;
+import aston.correction.entity.Stagiaire;
 import aston.correction.service.PersonneService;
 import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -24,10 +27,57 @@ public class PersonneController {
     }
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.FOUND)
     public Personne findById(@PathVariable Long id){
-
+        return this.personneService.findById(id);
     }
+
+    @GetMapping("nom/{nom}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Personne> findByNom(@PathVariable String nom){
+        return this.personneService.findByNom(nom);
+    }
+    @GetMapping("ville/{ville}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Personne> findAllByVille(@PathVariable String ville){
+        return this.personneService.findAllByVille(ville);
+    }
+    @GetMapping("formateurs/ville/{ville}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Formateur> findAllFormateurByMatiere (@PathVariable String nom){
+        return this.personneService.findAllFormateurByMatiere(nom);
+    }
+
+    @GetMapping("formateurs")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Formateur> findFormateur(){
+        return this.personneService.findFormateur();
+    }
+
+    @GetMapping("stagiaires")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Stagiaire> findAllStagiaire(){
+        return this.personneService.findAllStagiaire();
+    }
+
+    @GetMapping("formateurs/exp/{experience}")
+    public List<Formateur> findAllFormateurExperienceGreaterThan( @PathVariable int experience){
+        return this.personneService.findAllFormateurExperienceGreaterThan(experience);
+    }
+
+
+    @PostMapping("/stagiaire")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Personne createStagiaire (@RequestBody Stagiaire stagiaire){
+        return this.personneService.createStagiaire(stagiaire);
+    }
+    @PostMapping("/formateur")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Personne createFormateur (@RequestBody Formateur formateur){
+        return this.personneService.createFormateur(formateur);
+    }
+
+
 
 
 }
--- src/main/java/aston/correction/service/PersonneService.java
+++ src/main/java/aston/correction/service/PersonneService.java
@@ -1,16 +1,68 @@
 package aston.correction.service;
 
+import aston.correction.entity.Formateur;
 import aston.correction.entity.Personne;
+import aston.correction.entity.Stagiaire;
 import aston.correction.repository.PersonneRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonneService {
 
+    @Autowired
     private PersonneRepo personneRepo;
     public List<Personne> findAll() {
         return this.personneRepo.findAll();
+    }
+
+
+    public Personne findById(Long id) {
+        Optional<Personne> optionalPersonne = this.personneRepo.findById(id);
+        if (optionalPersonne.isPresent()) {
+            return optionalPersonne.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public Personne createFormateur(Formateur formateur) {
+        return this.personneRepo.save(formateur);
+    }
+
+    public Personne createStagiaire(Stagiaire stagiaire) {
+        return this.personneRepo.save(stagiaire);
+    }
+
+    public List<Personne> findByNom(String nom) {
+        return this.personneRepo.findByNom(nom);
+    }
+
+    public List<Personne> findAllByVille(String ville) {
+        return this.personneRepo.findAllByVille(ville);
+    }
+
+    public List<Formateur> findAllFormateurByMatiere(String nom) {
+        return this.personneRepo.findAllFormateurByMatiere(nom);
+    }
+
+
+    public List<Formateur> findFormateur() {
+        return this.personneRepo.findFormateur();
+    }
+
+
+    public List<Stagiaire> findAllStagiaire() {
+        return this.personneRepo.findAllStagiaire();
+    }
+
+
+    public List<Formateur> findAllFormateurExperienceGreaterThan(int experience) {
+        return this.personneRepo.findAllFormateurExperienceGreaterThan(experience);
     }
 }
